4ed87f890b2fa3d90bd3834a0290f233de1f488e
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-03-22 15:26:00 +0100

    Generate extra patient attributes

--- symptomtracker-backend/gen_fake_data.js
+++ symptomtracker-backend/gen_fake_data.js
@@ -6,30 +6,21 @@ faker.locale = "de";
 
 const mainSymptoms = ["Fieber", "Husten", "Kurzatmigkeit", "Muskel-/Gelenkschmerzen", "Halsschmerzen", "Kopfschmerzen", "Übelkeit / Erbrechen"];
 const otherSymptoms = ["Verstopfte Nase", "Durchfall", "Gliederschmerzen", "Appetitlosigkeit", "Gewichtsverlust", "Bauchschmerzen", "Konjunktivitis", "Hautausschlag", "Lymphknotenschwellung", "Apathie", "Somnolenz"];
+const {symptomCatalogue, _} = require('./gen_symptom_catalogue');
 
 function generateSymptomJourney(duration) {
     let symptomJourney = [];
     for (let i = 0; i < duration; i++) {
         let createdAt = faker.date.past((duration - i));
-        for (let j = 0; j < mainSymptoms.length; j++) {
+        for (let j = 0; j < symptomCatalogue.length; j++) {
+            let symptom = symptomCatalogue[j];
             symptomJourney.push(
                 {
-                    "description": mainSymptoms[j],
+                    "description": symptom.description,
                     "createdAt": createdAt,
-                    "@Category": "Hauptsymptome",
+                    "@Category": symptom["@Category"],
                     "comment": "",
-                    "symptomSeverity": faker.random.arrayElement(["0", "1", "2", "3", "4"]),
-                }
-            );
-        }
-        for (let j = 0; j < otherSymptoms.length; j++) {
-            symptomJourney.push(
-                {
-                    "description": otherSymptoms[j],
-                    "createdAt": createdAt,
-                    "@Category": "Weitere Symptome",
-                    "comment": "",
-                    "symptomSeverity": faker.random.arrayElement(["0", "1", "2", "3", "4"]),
+                    "symptomSeverity": faker.random.arrayElement(symptom.symptomSeverity),
                 }
             );
         }
@@ -37,11 +28,23 @@ function generateSymptomJourney(duration) {
     return symptomJourney;
 }
 
+const contact_date = new Date();
+contact_date.setDate(contact_date.getDate() - faker.random.number(10));
+const gender = faker.random.arrayElement(["m", "f", "d"]);
+
 db.add_patient({
     "name": faker.name.findName(),
     "age": faker.random.number(90),
-    "gender": faker.random.arrayElement(["m", "f", "d"]),
+    "gender": gender,
     "city": faker.address.city(),
     "symptomJourney": generateSymptomJourney(faker.random.number(14)),
     "linkedCode": new Date().toISOString(),
+    "geo_risk": faker.random.arrayElement([false, true]),
+    "contact_risk": faker.random.arrayElement([false, true]),
+    "contact_date": contact_date,
+    "work": "", //TODO
+    "smoker": faker.random.arrayElement([false, true]),
+    "preexisting_conditions": [],   //TODO
+    "immunal_suppressors": faker.random.arrayElement([false, true]),
+    "pregnant": (gender === "f" || gender === "d") && faker.random.arrayElement([false, true])
 }, (p) => console.log(p.ops[0]));
